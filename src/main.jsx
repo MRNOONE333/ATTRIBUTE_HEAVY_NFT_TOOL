be8538fcfe244e18ab1285514d8d96e2f4b0b102
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2025-01-11 02:37:35 +0530

    adding to git pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import NftAiTool from "./components/NftAiTool";
-import NftCustomTool from "./components/NftCustomTool"; // Import the new component
+// import NftAiTool from "./components/NftAiTool";
+// import NftCustomTool from "./components/NftCustomTool";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,8 +11,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/nft-ai-tool" element={<NftAiTool />} />
-        <Route path="/nft-custom-tool" element={<NftCustomTool />} /> {/* Add the new route */}
+        {/* <Route path="/nft-ai-tool" element={<NftAiTool />} />
+        <Route path="/nft-custom-tool" element={<NftCustomTool />} />  */}
       </Routes>
     </Router>
   </React.StrictMode>
