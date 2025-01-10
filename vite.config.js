4f41018b5363c11dca0189bb6761b1cc581d0ea2
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2025-01-11 01:37:48 +0530

    adding to git pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
-  // base: process.env.NODE_ENV === 'production' ? '/ATTRIBUTE_HEAVY_NFT_TOOL/' : '/',
+  base: '/ATTRIBUTE_HEAVY_NFT_TOOL/',
   plugins: [react()],
 })
